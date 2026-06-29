921b6702bd4c93e63ef4c1490eba826ab9a272b8
hannahpersis29-spec <hannahpersis29@gmail.com>
2026-06-29 22:23:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     
 def main():
-    application = Application.builder().token(BOT_TOKEN).build
+    application = Application.builder().token(BOT_TOKEN).build() 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
